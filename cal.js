d5162d1520492b9ac16d0e011b20c3804384aff2
Nasmeen MNM <121964946+nasmeenmnm@users.noreply.github.com>
2023-05-27 11:48:26 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,7 @@
+function calculate(){
+ 
+    let fn=parseFloat(document.getElementById("txtfirstnumber").value);
+    let sn=parseFloat(document.getElementById("txtsecondnumber").value);
+    
+    document.getElementById("Result").value=fn+sn;
+}
